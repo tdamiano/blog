3e4de9267a38a112042571d6d22172fbd5153089
Tony Damiano <damia025@umn.edu>
2017-07-19 15:03:59 -0500

    added datawrapper export.

--- ahprod/ah_prod.R
+++ ahprod/ah_prod.R
@@ -50,6 +50,61 @@ afflev_dw <- ahprod %>%
 write_csv(afflev_dw, "./data/afflev_dw.csv")
 
 
+### Cost burden Analysis ###
+cbrent <- readxl::read_excel("./data/region_cb.xlsx", sheet = 1) %>%
+cbown <- readxl::read_excel("./data/region_cb.xlsx", sheet = 2)
 
 
+#Renters 
+# by income level
+cbbyinc <- cbrent %>% 
+  filter(lowincome == "Y") %>%
+  mutate(
+    incomeband = factor(incomeband,  levels = c("Less than $10,000", "$10,000 to $19,999", "$20,000 to $34,999", "$35,000 to $49,999"))
+  ) %>% 
+  group_by(incomeband) %>%
+  summarise(
+    tothh  = sum(nhh),
+    ncb    = sum(nhh[costburden == "Y"]),
+    pct_cb  = (ncb/tothh)*100,
+    nxcb   = sum(nhh[excostburden == "Y"]),
+    pctxcb = (nxcb/tothh)*100
+  )
+  
+# total
+cbtotal <- cbrent %>% 
+  filter(lowincome == "Y") %>%
+  mutate(
+    incomeband = factor(incomeband,  levels = c("Less than $10,000", "$10,000 to $19,999", "$20,000 to $34,999", "$35,000 to $49,999"))
+  ) %>% 
+  summarise(
+    tothh  = sum(nhh),
+    ncb    = sum(nhh[costburden == "Y"]),
+    pct_cb  = (ncb/tothh)*100,
+    nxcb   = sum(nhh[excostburden == "Y"]),
+    pctxcb = (nxcb/tothh)*100
+  ) %>% mutate(incomeband = "All")
 
+x <- cbrent %>% 
+  filter(lowincome == "Y") %>%
+  mutate(
+    incomeband = factor(incomeband,  levels = c("Less than $10,000", "$10,000 to $19,999", "$20,000 to $34,999", "$35,000 to $49,999"))
+  ) %>% group_by(afflevel, incomeband) %>% 
+  summarise(
+    totunits = sum(nhh)
+  )
+
+# by income level crosstab
+xtab_dw <- cbrent %>% 
+  mutate(
+    afflevel = ifelse(afflevel %in% c("Less than 20.0 Percent","20.0 to 24.9 Percent","25.0 to 29.9 Percent"), "nocb",
+                      ifelse(afflevel %in% c("30.0 to 34.9 Percent", "35.0 to 39.9 Percent", "40.0 to 49.9 Percent"), "cb",
+                             ifelse(afflevel == "50.0 Percent or More", "xcb", NA)))
+  ) %>% 
+  group_by(incomeband, afflevel) %>% 
+  summarise(
+    nhh = sum(nhh, na.rm = TRUE)
+  ) %>% spread(afflevel, nhh) %>% 
+  select(-`<NA>`) %>% drop_na(incomeband)
+
+write_csv(xtab_dw, "./data/xtab_dw.csv")
